8b4dc1c927370fff41f036fb326cf0f14fa4ecfb
kevinstiebich <kevin.stiebich@gmail.com>
2026-09-23 11:47:25 +0200

    operator overridings hinzufügen

--- coinstack.dart
+++ coinstack.dart
@@ -1,3 +1,58 @@
 class CoinStack {
-  List<int> coins = [1, 2, 5, 10, 20, 50];
+  List<int> coins;
+  int get value => coins.fold(0, (a, b) => a + b);
+
+  CoinStack({required this.coins});
+
+  bool operator >(Object other) {
+    if (other is CoinStack) {
+      return value > other.value;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator <(Object other) {
+    if (other is CoinStack) {
+      return value < other.value;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator >=(Object other) {
+    if (other is CoinStack) {
+      return value >= other.value;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator <=(Object other) {
+    if (other is CoinStack) {
+      return value <= other.value;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator ==(Object other) {
+    if (other is CoinStack) {
+      return value == other.value;
+    } else {
+      return false;
+    }
+  }
+
+  CoinStack operator +(CoinStack other) {
+    return CoinStack(coins: [...coins, ...other.coins]);
+  }
+
+  CoinStack? operator -(CoinStack other) {
+    if (value > other.value) {
+      return CoinStack(coins: [...coins, ...other.coins]);
+    } else {
+      return null;
+    }
+  }
 }
--- main.dart
+++ main.dart
@@ -1,3 +1,9 @@
 import "coinstack.dart";
 
-main() {}
+main() {
+  var stack1 = CoinStack(coins: [1, 2, 5, 10]);
+  var stack2 = CoinStack(coins: [5, 10, 20, 10]);
+  var stack3 = stack1 + stack2;
+
+  print('${stack3.value}');
+}
